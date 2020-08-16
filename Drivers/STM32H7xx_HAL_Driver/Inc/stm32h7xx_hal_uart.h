d677290cba7611551732a111c0f24720dfb66e6c
vivek <vivek@ngxtechnologies.com>
2020-08-16 22:26:16 +0530

    warnings removed

--- Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart.h
+++ Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart.h
@@ -1574,6 +1574,7 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart);
 void HAL_UART_AbortCpltCallback(UART_HandleTypeDef *huart);
 void HAL_UART_AbortTransmitCpltCallback(UART_HandleTypeDef *huart);
 void HAL_UART_AbortReceiveCpltCallback(UART_HandleTypeDef *huart);
+HAL_StatusTypeDef HAL_UART_GetDMACount(UART_HandleTypeDef *huart, uint16_t *size);
 
 /**
   * @}
